e2a73bdfa20e242928dfd8f0b829f99bf71a3763
=sachinsingh33 <=ds76619@gmail.com>
2016-09-25 13:34:59 +0530

    first commit

---
+++ app/Http/Controllers/PageController.php
@@ -0,0 +1,123 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+use App\Http\Requests;
+
+use App\User;
+
+class PageController extends Controller
+{
+  public function Home()
+  {
+   $pagetype="index";
+   return view('pages.index') ->with(['pagetype'=> $pagetype]);
+  }
+
+  public function services()
+  {
+   $pagetype="services";
+   return view('pages.services') ->with(['pagetype'=> $pagetype]);
+  }
+  public function portfolio()
+  {
+   $pagetype="portfolio";
+   return view('pages.portfolio') ->with(['pagetype'=> $pagetype]);
+  }
+  public function shortcodes()
+  {
+   $pagetype="short-codes";
+   return view('pages.short-codes') ->with(['pagetype'=> $pagetype]);
+  }
+  public function mail()
+  {
+   $pagetype="mail";
+   return view('pages.mail') ->with(['pagetype'=> $pagetype]);
+  }
+   public function single()
+  {
+   $pagetype="single";
+   return view('pages.single') ->with(['pagetype'=> $pagetype]);
+  }
+  public function Home1()
+  {
+   $pagetype="Home1";
+   return view('pages.Home1') ->with(['pagetype'=> $pagetype]);
+  }
+  public function Mobilephones()
+  {
+   $pagetype="Mobilephones";
+   return view('pages.Mobilephones') ->with(['pagetype'=> $pagetype]);
+  }
+  public function Tablets()
+  {
+   $pagetype="Tablets";
+   return view('pages.Tablets') ->with(['pagetype'=> $pagetype]);
+  }
+public function Laptops()
+  {
+   $pagetype="Laptops";
+   return view('pages.Laptops') ->with(['pagetype'=> $pagetype]);
+  }
+public function Motherboards()
+  {
+   $pagetype="Motherboards";
+   return view('pages.Motherboards') ->with(['pagetype'=> $pagetype]);
+  }
+public function Processors()
+  {
+   $pagetype="Processors";
+   return view('pages.Processors') ->with(['pagetype'=> $pagetype]);
+  }
+public function Offers()
+  {
+   $pagetype="Offers";
+   return view('pages.Offers') ->with(['pagetype'=> $pagetype]);
+  }
+public function SignUp()
+  {
+   $pagetype="SignUp";
+   return view('pages.SignUp') ->with(['pagetype'=> $pagetype]);
+  }
+public function Login()
+  {
+   $pagetype="Login";
+   return view('pages.Login') ->with(['pagetype'=> $pagetype]);
+  }
+public function form()
+  {
+   return view('pages.form') ;
+  }
+public function LogoutView()
+  {
+   return view('auth.Login');
+  }
+public function store(Request $input)
+  {
+    User::create($input->all());
+   /*$pagetype="Login";
+   return view('pages.Login') ->with(['pagetype'=> $pagetype]);*/
+   var_dump("success");
+  }
+public function Withdraw()
+  {
+   return view('Withdraw') ;
+  }
+  public function History()
+  {
+   return view('History') ;
+  }
+  public function Transfer()
+  {
+   return view('Transfer') ;
+  }
+  public function Deposit()
+  {
+   return view('Deposit') ;
+  }
+
+
+
+}
